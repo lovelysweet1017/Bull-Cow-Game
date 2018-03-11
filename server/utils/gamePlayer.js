2b087954dc4d08c0f8dd9f0813cd8a3a2fce65d5
lovelysweet1017 <simmons4681@gmail.com>
2018-03-11 12:47:49 -0500

    server guessing algorithm improved

--- server/utils/gamePlayer.js
+++ server/utils/gamePlayer.js
@@ -5,9 +5,9 @@ var makeGuess = (lastGuess, bull, cow) => {
         global.availableDigits = global.availableDigits.replace(lastGuess[1], '');
         global.availableDigits = global.availableDigits.replace(lastGuess[2], '');
         console.log(global.availableDigits);
-        var num = random3Digit(global.availableDigits);
+        var num = random3Digit(global.availableDigits, 3);
         while (global.usedNumbers.indexOf(num) != -1) {
-            num = random3Digit(global.availableDigits);
+            num = random3Digit(global.availableDigits, 3);
         }
         global.usedNumbers.push(num);
         return num;
@@ -16,19 +16,55 @@ var makeGuess = (lastGuess, bull, cow) => {
         console.log(lastGuess)
     } else if ((bull + cow) === 3) {
         global.availableDigits = (lastGuess + '');
-        var num = random3Digit(global.availableDigits);
+        var num = random3Digit(global.availableDigits, 3);
         while (global.usedNumbers.indexOf(num) != -1) {
-            num = random3Digit(global.availableDigits);
+            num = random3Digit(global.availableDigits, 3);
+        }
+        global.usedNumbers.push(num);
+        return num;
+    } else if (!global.cow && (cow + bull) === 2) {
+        global.cow = true;
+        global.savedDigit = lastGuess[2];
+        // global.savedState = global.availableDigits;
+        let temp = random3Digit(global.availableDigits, 1);
+        while (lastGuess[0] == temp || lastGuess[1] == temp || lastGuess[2] == temp) {
+            temp = random3Digit(global.availableDigits, 1);
+        }
+        return lastGuess.replace(lastGuess[2], temp);;
+    } else if (global.cow && (cow + bull) === 2) {
+        global.cow = false;
+        global.availableDigits = global.availableDigits.replace(global.savedDigit, '');
+        let temp = random3Digit(global.availableDigits, 1);
+        while (lastGuess[0] == temp || lastGuess[1] == temp || lastGuess[2] == temp) {
+            temp = random3Digit(global.availableDigits, 1);
+        }
+        var num = lastGuess.replace(lastGuess[2], '') + temp;
+        while (global.usedNumbers.indexOf(num) != -1) {
+            num = random3Digit(global.availableDigits, 3);
+        }
+        global.usedNumbers.push(num);
+        return num;
+    }
+    else if (global.cow && (cow + bull) < 2) {
+        global.cow = false;
+        var num = random3Digit(global.availableDigits, 3);
+        while (global.usedNumbers.indexOf(num) != -1) {
+            num = random3Digit(global.availableDigits, 3);
         }
         global.usedNumbers.push(num);
         return num;
     }
     else {
-        return random3Digit(global.availableDigits);
+        var num = random3Digit(global.availableDigits, 3);
+        while (global.usedNumbers.indexOf(num) != -1) {
+            num = random3Digit(global.availableDigits, 3);
+        }
+        global.usedNumbers.push(num);
+        return num;
     }
 }
-var random3Digit = function (numberList) {
-    return shuffle(numberList.split('')).join('').substring(0, 3);
+var random3Digit = function (numberList, n) {
+    return shuffle(numberList.split('')).join('').substring(0, n);
 }
 
 function shuffle(o) {
